b12468100953b9be8484ad70da5e020a0ee895f3
= <=>
2023-03-26 08:46:41 +0200

    Modify models transaction

--- spotato/spotato_app/models.py
+++ spotato/spotato_app/models.py
@@ -37,5 +37,5 @@ class Requete(models.Model):
 class Transaction(models.Model):
     montant = models.FloatField()
     source = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_transaction_source')
-    destination = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_transaction_destination')
+    requete = models.ForeignKey(Requete, on_delete=models.DO_NOTHING, related_name="transaction_requete")
     date_time = models.DateTimeField(default=timezone.now())
